a1a636e8be543dbee597cf4a14dcb4383935dcf7
murych <murychkun@yahoo.com>
2016-08-06 16:46:37 +0300

    updated urls, preparing to webapge itself

--- lambdaweb/urls.py
+++ lambdaweb/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.contrib.sitemaps.views import sitemap
+# from .sitemap import *
+
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'lambdaweb.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+	# Examples:
+	# url(r'^$', 'lambdaweb.views.home', name='home'),
+	# url(r'^blog/', include('blog.urls')),
+	url(r'^registry/', include('registry.urls')),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', views.index, name='index'),
 ]
