114ded4546a84dfbdb2e6c7143832b3257d89364
murych <murychkun@yahoo.com>
2016-08-08 13:58:22 +0300

    social links are now part of `base.html`

--- registry/views.py
+++ registry/views.py
@@ -4,19 +4,19 @@ from .models import *
 
 class PartnerView(generic.DetailView):
 	model = Partner
-	template_name = 'partner_detail.html'
+	template_name = 'details/partner_details.html'
 
 
 class MemberView(generic.DetailView):
 	model = Member
-	template_name = 'member_detail.html'
+	template_name = 'details/member_details.html'
 
 
 class ProjectView(generic.DetailView):
 	model = Project
-	template_name = 'project_detail.html'
+	template_name = 'details/project_details.html'
 
 
 class EventView(generic.DetailView):
 	model = Event
-	template_name = 'event_detail.html'
+	template_name = 'details/event_details.html'
