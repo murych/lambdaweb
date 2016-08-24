797ded24f48aca13dfa3fa0821631d3f6ef615cf
Timur <murychkun@yahoo.com>
2016-08-24 23:54:57 +0400

    Added error pages (#7)
    * added 404 handler #5
    
    * updated 404 handler #5
    
    * added 500 handler #5
    
    * added 403 handler #5
    
    * emergency fix on 404 handler #5

--- lambdaweb/views.py
+++ lambdaweb/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render_to_response
 from registry.models import *
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 
 def index(request):
@@ -29,3 +30,21 @@ def partners(request):
 def projects(request):
 	projects_list = Project.objects.all()
 	return render_to_response('projects.html', {'projects_list': projects_list})
+
+
+def handler403(request):
+	response = render_to_response('403.html', {}, context_instance=RequestContext(request))
+	response.status_code = 403
+	return response
+
+
+def handler404(request):
+	response = render_to_response('404.html', {}, context_instance=RequestContext(request))
+	response.status_code = 404
+	return response
+
+
+def handler500(request):
+	response = render_to_response('500.html', {}, context_instance=RequestContext(request))
+	response.status_code = 500
+	return response
