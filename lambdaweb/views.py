da992f21e66cb11e84989147224f4163d4180b0e
murych <murychkun@yahoo.com>
2016-08-24 21:27:58 +0300

    added 404 handler #5

--- lambdaweb/views.py
+++ lambdaweb/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render_to_response
 from registry.models import *
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 
 def index(request):
@@ -29,3 +30,15 @@ def partners(request):
 def projects(request):
 	projects_list = Project.objects.all()
 	return render_to_response('projects.html', {'projects_list': projects_list})
+
+
+def handler404(request):
+	response = render_to_response('404.html', {}, context_instance=RequestContext(request))
+	response.status_code = 404
+	return response
+
+
+def handler500(request):
+	response = render_to_response('500.html', {}, context_instance=RequestContext(request))
+	response.status_code = 500
+	return response
