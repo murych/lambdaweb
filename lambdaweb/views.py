b24109ff44b4917a69a4d20e8baf90a415bd994e
murych <murychkun@yahoo.com>
2016-08-24 22:02:42 +0300

    added 403 handler #5

--- lambdaweb/views.py
+++ lambdaweb/views.py
@@ -32,6 +32,12 @@ def projects(request):
 	return render_to_response('projects.html', {'projects_list': projects_list})
 
 
+def handler403(request):
+	response = render_to_response('403.html', {}, context_instance=RequestContext(request))
+	response.status_code = 403
+	return response
+
+
 def handler404(request):
 	response = render_to_response('404.html', {}, context_instance=RequestContext(request))
 	response.status_code = 404
