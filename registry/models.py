8e0f0067636f47ff5502a6b6ab1d9d28a020d62f
murych <murychkun@yahoo.com>
2016-09-01 23:29:27 +0300

    correct paths for media files according to #3

--- registry/models.py
+++ registry/models.py
@@ -1,21 +1,29 @@
-from django.db import models
+from datetime import date
+
 from django.core.urlresolvers import reverse
-from unidecode import unidecode
+from django.db import models
 from django.template.defaultfilters import slugify
-# from filer.models.foldermodels import Folder
 from django_markdown.models import MarkdownField
+from unidecode import unidecode
 
 default_url_vk = "https://vk.com/lambdafrela"
 default_url_github = "https://github.com/lambdafrela"
 
 
+def get_image_dir_path(instance, filename):
+	if instance.get_type == 'Event':
+		return 'events/{0}/{1}/{2}'.format(date.today().year, instance.slug, filename)
+	else:
+		return '{0}/{1}'.format(instance.get_type, instance.slug)
+
+
 class Entry(models.Model):
-	name = models.CharField(max_length=200)
-	url = models.URLField(max_length=100, default=default_url_vk)
 	description = MarkdownField()
+	name = models.CharField(max_length=200)
 	slug = models.SlugField(max_length=300, blank=True)
 	created_date = models.DateTimeField(auto_now_add=True)
-	image = models.ImageField(null=True)
+	url = models.URLField(max_length=100, default=default_url_vk)
+	image = models.ImageField(null=True, upload_to=get_image_dir_path)
 
 	class Meta:
 		ordering = ['-created_date']
@@ -28,17 +36,26 @@ class Entry(models.Model):
 			self.slug = slugify(unidecode(self.name))
 		super(Entry, self).save(*args, **kwargs)
 
+	@property
+	def get_type(self):
+		return self.__class__.__name__
+
 
 class Event(Entry):
 	date = models.DateTimeField()
-	address = models.CharField(max_length=300)
-	future = models.BooleanField(default=False)
-	avatar = models.ImageField(null=True, blank=True, upload_to='events/%Y/%m/%d/')
-	speaker = models.CharField(max_length=100, blank=True)
 	calendar = models.URLField(blank=True)
+	address = models.CharField(max_length=300)
+	speaker = models.CharField(max_length=100, blank=True)
+	avatar = models.ImageField(null=True, blank=True, upload_to=get_image_dir_path)
 
 	class Meta:
 		ordering = ['-date']
+
+	@property
+	def is_future(self):
+		if date.today() < self.date:
+			return True
+		return False
 
 
 class Project(Entry):
@@ -50,15 +67,15 @@ class Partner(Entry):
 
 
 class Member(Entry):
-	github = models.URLField(max_length=100, default=default_url_github)
 	email = models.EmailField(max_length=100)
+	github = models.URLField(max_length=100, default=default_url_github)
 
 
 class Feedback(models.Model):
-	date = models.DateTimeField(auto_now_add=True)
+	role = models.CharField(max_length=50)
 	text = models.TextField(max_length=100)
 	author = models.CharField(max_length=50)
-	role = models.CharField(max_length=50)
+	date = models.DateTimeField(auto_now_add=True)
 
 	class Meta:
 		ordering = ['-date']
