e5224557b9fa90eacffbefed106f31352c9c4864
murych <murychkun@yahoo.com>
2017-01-13 21:23:11 +0300

    сменил язык проекта на русский :muscule:

--- lambdaweb/settings.py
+++ lambdaweb/settings.py
@@ -107,11 +107,11 @@ USE_TZ = True
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = "en"
+LANGUAGE_CODE = "ru-RU"
 
 # Supported languages
 LANGUAGES = (
-	('en', _('English')),
+	('ru-RU', _('Russian')),
 )
 
 # A boolean that turns on/off debug mode. When set to ``True``, stack traces
