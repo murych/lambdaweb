87cc6d268b1a0a17357fe29831c7df859ce8a804
murych <murychkun@yahoo.com>
2017-02-03 00:07:27 +0300

    passing templates around #28

--- gulpfile.js
+++ gulpfile.js
@@ -17,19 +17,19 @@ var gulp           = require('gulp'),
 // Скрипты проекта
 gulp.task('scripts', function() {
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.js', // Всегда в конце
+		'frontend/templates/libs/jquery/dist/jquery.min.js',
+		'frontend/templates/js/common.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
 	.pipe(uglify())
-	.pipe(gulp.dest('app/js'))
+	.pipe(gulp.dest('frontend/templates/js'))
 	.pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('browser-sync', function() {
 	browserSync({
 		server: {
-			baseDir: 'app'
+			baseDir: 'frontend/templates'
 		},
 		notify: false,
 		// tunnel: true,
@@ -38,25 +38,25 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('sass', function() {
-	return gulp.src('app/sass/**/*.sass')
+	return gulp.src('frontend/templates/sass/**/*.sass')
 	.pipe(sass({
 		includePaths: bourbon.includePaths
 	}).on("error", notify.onError()))
 	.pipe(rename({suffix: '.min', prefix : ''}))
 	.pipe(autoprefixer(['last 15 versions']))
 	.pipe(cleanCSS())
-	.pipe(gulp.dest('app/css'))
+	.pipe(gulp.dest('frontend/templates/css'))
 	.pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('watch', ['sass', 'scripts', 'browser-sync'], function() {
 	gulp.watch('app/sass/**/*.sass', ['sass']);
-	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['scripts']);
-	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch(['libs/**/*.js', 'frontend/templates/js/common.js'], ['scripts']);
+	gulp.watch('frontend/templates/*.html', browserSync.reload);
 });
 
 gulp.task('imagemin', function() {
-	return gulp.src('app/img/**/*')
+	return gulp.src('frontend/templates/img/**/*')
 	.pipe(cache(imagemin()))
 	.pipe(gulp.dest('dist/img')); 
 });
@@ -64,20 +64,20 @@ gulp.task('imagemin', function() {
 gulp.task('build', ['removedist', 'imagemin', 'sass', 'scripts'], function() {
 
 	var buildFiles = gulp.src([
-		'app/*.html',
-		'app/.htaccess',
+		'frontend/templates/*.html',
+		'frontend/templates/.htaccess',
 		]).pipe(gulp.dest('dist'));
 
 	var buildCss = gulp.src([
-		'app/css/main.min.css',
+		'frontend/templates/css/main.min.css',
 		]).pipe(gulp.dest('dist/css'));
 
 	var buildJs = gulp.src([
-		'app/js/scripts.min.js',
+		'frontend/templates/js/scripts.min.js',
 		]).pipe(gulp.dest('dist/js'));
 
 	var buildFonts = gulp.src([
-		'app/materialdesignicons/**/*',
+		'frontend/templates/materialdesignicons/**/*',
 		]).pipe(gulp.dest('dist/materialdesignicons'));
 
 });
