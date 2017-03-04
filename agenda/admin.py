0bd267c0dab9b4317ea5716d04b39cffb1f140c4
murych <murychkun@yahoo.com>
2017-03-05 02:55:41 +0300

    agenda app code style fixed

--- agenda/admin.py
+++ agenda/admin.py
@@ -16,45 +16,46 @@ event_fieldsets[0][1]["fields"].insert(3, "facebook_event")
 event_fieldsets[0][1]["fields"].extend(["content", "allow_comments"])
 event_list_display = ["title", "user", "status", "admin_link"]
 if settings.EVENT_USE_FEATURED_IMAGE:
-	event_fieldsets[0][1]["fields"].insert(-2, "featured_image")
-	event_list_display.insert(0, "admin_thumb")
+    event_fieldsets[0][1]["fields"].insert(-2, "featured_image")
+    event_list_display.insert(0, "admin_thumb")
 event_fieldsets = list(event_fieldsets)
 event_list_filter = deepcopy(DisplayableAdmin.list_filter) + ("location",)
 
 
 class EventAdmin(DisplayableAdmin, OwnableAdmin):
-	"""
-	Admin class for events.
-	"""
+    """
+    Admin class for events.
+    """
 
-	fieldsets = event_fieldsets
-	list_display = event_list_display
-	list_filter = event_list_filter
+    fieldsets = event_fieldsets
+    list_display = event_list_display
+    list_filter = event_list_filter
 
-	def save_form(self, request, form, change):
-		"""
-		Super class ordering is important here - user must get saved first.
-		"""
-		OwnableAdmin.save_form(self, request, form, change)
-		return DisplayableAdmin.save_form(self, request, form, change)
+    def save_form(self, request, form, change):
+        """
+        Super class ordering is important here - user must get saved first.
+        """
+        OwnableAdmin.save_form(self, request, form, change)
+        return DisplayableAdmin.save_form(self, request, form, change)
 
 
 class EventLocationAdmin(admin.ModelAdmin):
-	"""
-	Admin class for event locations. Hides itself from the admin menu
-	unless explicitly specified.
-	"""
+    """
+    Admin class for event locations. Hides itself from the admin menu
+    unless explicitly specified.
+    """
 
-	fieldsets = ((None, {"fields": ("title", "address", "mappable_location", "lat", "lon")}),)
+    fieldsets = ((None, {
+        "fields": ("title", "address", "mappable_location", "lat", "lon")}),)
 
-	def in_menu(self):
-		"""
-		Hide from the admin menu unless explicitly set in ``ADMIN_MENU_ORDER``.
-		"""
-		for (name, items) in settings.ADMIN_MENU_ORDER:
-			if "agenda.EventLocation" in items:
-				return True
-		return False
+    def in_menu(self):
+        """
+        Hide from the admin menu unless explicitly set in ``ADMIN_MENU_ORDER``.
+        """
+        for (name, items) in settings.ADMIN_MENU_ORDER:
+            if "agenda.EventLocation" in items:
+                return True
+        return False
 
 
 admin.site.register(Event, EventAdmin)
