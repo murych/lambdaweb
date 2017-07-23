681f82a913b15d91040e41788ff33868e43eda11
Euphe <euphetar@gmail.com>
2017-07-23 19:42:05 +0300

    add cache

--- sites/urls.py
+++ sites/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^list$', views.article_list, name="article_list"),
-    url(r'^posts/(?P<slug>[-\w]+)/$', views.article, name="article"),
-    url(r'^list/page/(?P<page>[-\d]+)$', views.article_list, name="article_list_page"),
-    url(r'^events$', views.event_list, name="event_list"),
-    url(r'^events/(?P<slug>[-\w]+)/$', views.event, name="event"),
-    url(r'^partners$', views.patners_list, name="patners_list"),
-    url(r'^partners/(?P<slug>[-\w]+)/$', views.patner, name="partner"),
+    url(r'^$', cache_page(60 * 3)(views.index), name='index'),
+    url(r'^list$', cache_page(60 * 0.5)(views.article_list), name="article_list"),
+    url(r'^posts/(?P<slug>[-\w]+)/$', cache_page(60 * 0.5)(views.article), name="article"),
+    url(r'^list/page/(?P<page>[-\d]+)$', cache_page(60 * 0.5)(views.article_list), name="article_list_page"),
+    url(r'^events$', cache_page(60 * 0.5)(views.event_list), name="event_list"),
+    url(r'^events/(?P<slug>[-\w]+)/$', cache_page(60 * 0.5)(views.event), name="event"),
+    url(r'^partners$', cache_page(60 * 3)(views.patners_list), name="patners_list"),
+    url(r'^partners/(?P<slug>[-\w]+)/$', cache_page(60 * 3)(views.patner), name="partner"),
 ]
